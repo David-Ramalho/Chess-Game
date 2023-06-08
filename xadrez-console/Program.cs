764a0c0ec17fecaaef4c81d4ad755e9688cbb521
David-Ramalho <tavaresramalho@hotmail.com>
2023-06-07 22:21:17 -0300

    Execptions and improviments.

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,14 +8,20 @@ namespace xadrez_console
 	{
 		static void Main(string[] args)
 		{
-			Tabuleiro tab = new Tabuleiro(8, 8);
+			try
+			{
+				Tabuleiro tab = new Tabuleiro(8, 8);
 
-			tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 3));
-			tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(2, 1));
-			tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(4, 5));
+				tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(5, 6));
+				tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(2, 1));
+				tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 3));
 
-			Tela.imprimirTabuleiro(tab);
-
+				Tela.imprimirTabuleiro(tab);
+			}
+			catch(TabuleiroException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
 			Console.ReadLine();
 		}
 	}
