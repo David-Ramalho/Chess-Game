e1d1c362549cbcaf6abd168b099fdd762c8b4fb6
David-Ramalho <tavaresramalho@hotmail.com>
2023-06-14 23:16:08 -0300

    update

--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -24,7 +24,65 @@ namespace xadrez
 		{
 			bool[,] mat = new bool[tab.linhas, tab.colunas];
 
+			Posicao pos = new Posicao(0, 0);
+
+			//Acima
+			pos.definirValores(posicao.linha - 1, posicao.coluna);
+			if(tab.posicaoValida(pos) && podeMover(pos))
+			{
+				mat[pos.linha,pos.coluna] = true;
+			}
+
+			//ne
+
+			pos.definirValores(posicao.linha - 1, posicao.coluna+1);
+			if (tab.posicaoValida(pos) && podeMover(pos))
+			{
+				mat[pos.linha, pos.coluna] = true;
+			}
+
+			//direita
+			pos.definirValores(posicao.linha , posicao.coluna+1);
+			if (tab.posicaoValida(pos) && podeMover(pos))
+			{
+				mat[pos.linha, pos.coluna] = true;
+			}
+
+			//se
+			pos.definirValores(posicao.linha +1 , posicao.coluna+1);
+			if (tab.posicaoValida(pos) && podeMover(pos))
+			{
+				mat[pos.linha, pos.coluna] = true;
+			}
+
+			//abaixo
+			pos.definirValores(posicao.linha + 1, posicao.coluna);
+			if (tab.posicaoValida(pos) && podeMover(pos))
+			{
+				mat[pos.linha, pos.coluna] = true;
+			}
+			//SO
+			pos.definirValores(posicao.linha + 1, posicao.coluna-1);
+			if (tab.posicaoValida(pos) && podeMover(pos))
+			{
+				mat[pos.linha, pos.coluna] = true;
+			}
+			//Esquerda
+			pos.definirValores(posicao.linha , posicao.coluna-1);
+			if (tab.posicaoValida(pos) && podeMover(pos))
+			{
+				mat[pos.linha, pos.coluna] = true;
+			}
+
+			//NO
+			pos.definirValores(posicao.linha - 1, posicao.coluna-1);
+			if (tab.posicaoValida(pos) && podeMover(pos))
+			{
+				mat[pos.linha, pos.coluna] = true;
+			}
+			return mat;
+
 		}
 	}
 }
-}
+
